882c6bdf346d2352946c7ffba29ef3f133f16a8d
Annika Monari <annikamonari@gmail.com>
2014-01-28 17:46:54 +0000

    file changes, errors with the last commit. Git was tracking the wrong version of the raytracer file. This commit has now added the intercept method for all types of spherical surfaces, including plane surfaces.

--- raytracer.py
+++ raytracer.py
@@ -45,10 +45,10 @@ class Ray:
 
 class OpticalElement:
   	def propagate(self, ray):
-    	"propagate a ray through the optical element"
-    	raise NotImplementedError()
+  		"""propagate a ray through the optical element"""
+  		raise NotImplementedError()
 
-class SphericalRefraction(OpticalElement):
+class SphericalRefraction:
 
 	def __init__(self, z0 = 0.0, c = 0.0, n1 = 1.0, n2 = 1.0, ar = 0.0):
 		self.__z0 = np.array(0.0, 0.0, z0)
@@ -56,26 +56,57 @@ class SphericalRefraction(OpticalElement):
 		self.__n1 = n1
 		self.__n2 = n2
 		self.__ar = ar
-		self.__Rvector = self.radialvector()
+		self.__r= self.radius()
+		self.__s = self.surface()
+		self.__centre = self.centre()
 		
 		# ar = aperture radius
 		# c = curvature
 
-	def radialvector(self):
-		return np.array(0.0, 0.0, -(1/self.__c))
-		#Rmag = abs(Rvector)
-		#return Rvector
+	def radius(self):
+		if self.__c == 0:
+			return float("inf")
+		else:
+			return abs(1/self.__c)
 
 	def surface(self):
 		if self.__c < 0:
-			return "concave surface"
+			return "concave"
 		elif self.__c > 0:
-			return "convex surface"
+			return "convex"
 		else:
-			return "plane surface"
+			return "plane"
+
+	def centre(self):
+		if self.__s == "convex":
+			return np.array(0, 0, self.__z0 + self.__r)
+		elif self.__s == "concave":
+			return np.array(0, 0, self.__z0 - self.__r)
+		else:
+			return "plane surfaces have no centre"
 
 	def intercept(self, ray):
-		
+		r = ray.p() - self.__centre
+		r_mag = np.sqrt(sum(n**2 for n in r))
+		rdotkunit = np.dot(r, ray.kunit())
+		l1 = -rdotkunit + np.sqrt(rdotkunit**2 - r_mag**2 + self.__r**2)
+		l2 = -rdotkunit - np.sqrt(rdotkunit**2 - r_mag**2 + self.__r**2)
+		lplane = (self.__z0 - ray.p()[2]) / ray.kunit()[2]
+
+		if (rdotkunit**2 - r_mag**2 + self.__r**2) < 0:
+			return None
+		elif (rdotkunit**2 - r_mag**2 + self.__r**2) == 0:
+			return ray.p() + -rdotkunit*ray.kunit()
+		elif self.__s == "convex": 
+			return ray.p() + min(l1, l2)*ray.kunit()
+		elif self.__s == "concave":
+			return ray.p() + max(l1, l2)*ray.kunit()
+		else:
+			return ray.p() + lplane*ray.kunit()
+
+
+
+
 
 
 		
