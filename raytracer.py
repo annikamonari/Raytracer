30015f8693a017b18458b0e5a80a1f925e7560fc
Annika Monari <annikamonari@gmail.com>
2014-02-03 17:49:58 +0000

    added some methods to help implement the refract method later on, and started the refract method; have yet to implement the production of the resulting direction vector in media II

--- raytracer.py
+++ raytracer.py
@@ -51,58 +51,70 @@ class OpticalElement:
 class SphericalRefraction:
 
 	def __init__(self, z0 = 0.0, c = 0.0, n1 = 1.0, n2 = 1.0, ar = 0.0):
-		self.__z0 = np.array(0.0, 0.0, z0)
-		self.__c = c
-		self.__n1 = n1
-		self.__n2 = n2
-		self.__ar = ar
-		self.__r= self.radius()
-		self.__s = self.surface()
-		self.__centre = self.centre()
+		self.z0 = np.array([0.0, 0.0, z0])
+		self.c = c
+		self.n1 = n1
+		self.n2 = n2
+		self.ar = ar
+		self.r= self.radius()
+		self.s = self.surface()
+		self.centre = self.centre()
 		
 		# ar = aperture radius
 		# c = curvature
 
 	def radius(self):
-		if self.__c == 0:
+		if self.c == 0:
 			return float("inf")
 		else:
-			return abs(1/self.__c)
+			return abs(1/self.c)
 
 	def surface(self):
-		if self.__c < 0:
+		if self.c < 0:
 			return "concave"
-		elif self.__c > 0:
+		elif self.c > 0:
 			return "convex"
-		elif self.__c == 0:
+		elif self.c == 0:
 			return "plane"
 
 	def centre(self):
-		if self.__s == "convex":
-			return np.array(0, 0, self.__z0 + self.__r)
-		elif self.__s == "concave":
-			return np.array(0, 0, self.__z0 - self.__r)
-		elif self.__s == "plane":
+		if self.s == "convex":
+			return np.array(0, 0, self.z0 + self.r)
+		elif self.s == "concave":
+			return np.array(0, 0, self.z0 - self.r)
+		elif self.s == "plane":
 			return "plane surfaces have no centre"
 
 	def intercept(self, ray):
-		r = ray.p() - self.__centre
+		r = ray.p() - self.centre
 		r_mag = np.sqrt(sum(n**2 for n in r))
 		rdotkunit = np.dot(r, ray.kunit())
-		l1 = -rdotkunit + np.sqrt(rdotkunit**2 - r_mag**2 + self.__r**2)
-		l2 = -rdotkunit - np.sqrt(rdotkunit**2 - r_mag**2 + self.__r**2)
-		lplane = (self.__z0 - ray.p()[2]) / ray.kunit()[2]
+		l1 = -rdotkunit + np.sqrt(rdotkunit**2 - r_mag**2 + self.r**2)
+		l2 = -rdotkunit - np.sqrt(rdotkunit**2 - r_mag**2 + self.r**2)
+		lplane = (self.z0 - ray.p()[2]) / ray.kunit()[2]
 
-		if (rdotkunit**2 - r_mag**2 + self.__r**2) < 0:
+		if (rdotkunit**2 - r_mag**2 + self.r**2) < 0:
 			return None
-		elif (rdotkunit**2 - r_mag**2 + self.__r**2) == 0:
+		elif (rdotkunit**2 - r_mag**2 + self.r**2) == 0:
 			return ray.p() + -rdotkunit*ray.kunit()
-		elif self.__s == "convex": 
+		elif self.s == "convex": 
 			return ray.p() + min(l1, l2)*ray.kunit()
-		elif self.__s == "concave":
+		elif self.s == "concave":
 			return ray.p() + max(l1, l2)*ray.kunit()
-		elif self.__s == "plane":
+		elif self.s == "plane":
 			return ray.p() + lplane*ray.kunit()
+
+	def unitsurfacenormal(self, ray):
+		Q = self.intercept(ray)
+		surface_normal = Q - self.centre()
+		return surface_normal/np.sqrt(sum(n**2 for n in surface_normal))
+
+	def refract(self, ray):
+		unit_n = self.unitsurfacenormal(ray)
+		unit_k = ray.kunit()
+		theta_1 = np.arccos(np.dot(unit_k, unit_n))
+		theta_2 = np.arcsin((self.n1*np.sin(theta_1)/self.n2))
+		
 
 
 
