493aceba538cd0af4bd90bbb7595bde74dfe9c9a
Annika Monari <annikamonari@gmail.com>
2014-02-10 15:48:42 +0000

    Added doc strings for less straightforward functions, and all classes, and rechose private vs public attributes

--- raytracer.py
+++ raytracer.py
@@ -9,63 +9,70 @@ optical ray tracer
 """
 
 def objectchecker(sphericalrefractionlist = None, outputplane = None, ray = None):
-	for i in sphericalrefractionlist:
-		if not isinstance(i, SphericalRefraction):
-			raise Exception("Surface input isn't SphericalRefraction surface")
-	if not isinstance(outputplane, OutputPlane):
-		raise Exception("Output plane input is not an Output plane")
-	if not isinstance(ray, Ray):
-		raise Exception("Ray input is not a ray object")
+	"""Check if an object passed as a parameter is the correct object"""
+	if sphericalrefractionlist is not None:
+		for i in sphericalrefractionlist:
+			if not isinstance(i, SphericalRefraction):
+				raise Exception("Surface input isn't SphericalRefraction surface")
+	if outputplane is not None:
+		if not isinstance(outputplane, OutputPlane):
+			raise Exception("Output plane input is not an Output plane")
+	if ray is not None:
+		if not isinstance(ray, Ray):
+			raise Exception("Ray input is not a ray object")
 
 class Ray:
+"""Create a ray with an initial point and direction"""
 
 	def __init__(self, p = None, k = None):
 		if p == None:
 			p = [0,0,0]
 		if k == None:
 			k = [0,0,0]
-		self.__points = [np.array(p)]
-		self.__directions = [np.array(k)/np.sqrt(sum(n**2 for n in k))]
+		self._points = [np.array(p)]
+		self._directions = [np.array(k)/np.sqrt(sum(n**2 for n in k))]
 		self.checklength()
 
 	def __repr__(self):
 		return "[x, y, z] = %s + %st" % (self.p(), self.k())
 	
 	def checklength(self):
-		if any(len(i) != 3 for i in self.__points + self.__directions):
+		if any(len(i) != 3 for i in self._points + self._directions):
 			raise Exception("Ray point or direction parameter size")
 
 	def points(self):
-		return self.__points
+		return self._points
 
 	def directions(self):
-		return self.__directions
+		return self._directions
 
 	def p(self):
-		return self.__points[len(self.__points)-1]
+		return self._points[len(self._points)-1]
 
 	def k(self):
-		return self.__directions[len(self.__directions)-1]
+		return self._directions[len(self._directions)-1]
 
 	def append(self, p = None , k = None):
 		if p is not None:
-			self.__points.append(np.array(p))
+			self._points.append(np.array(p))
 		if k is not None:
-			self.__directions.append(np.array(k))		
+			self._directions.append(np.array(k))		
 		self.checklength()
 
 	def vertices(self):
-		for item in self.__points: print item
+		return list(item for item in self._points)
 
-	# propagates and plots the ray through a sphericalrefracting surface and output plane
 	def trace(self, sphericalrefraction, outputplane):
+		"""propagates and plots the ray through a sphericalrefracting 
+		surface and output plane"""
 		objectchecker([sphericalrefraction], outputplane)
 		sphericalrefraction.propagate_ray(self)
 		outputplane.propagate_ray(self)
 		self.plot()
 
-	# estimates the paralaxial focus using 'trace' method for two rays and 1 surface
 	def parallaxtrace(self, ray, sphericalrefraction, outputplane):
+		"""estimates the parallaxial focus through 1 spherical
+		refraction lens using the trace() method for two rays"""
 		objectchecker([sphericalrefraction], outputplane, ray)
 		sphericalrefraction.propagate_ray(self)
 		sphericalrefraction.propagate_ray(ray)
@@ -73,8 +80,9 @@ class Ray:
 		outputplane.propagate_ray(ray)
 		self.plotparallax(ray)
 
-	# estimates the paralaxial focus using 'trace' method for two rays and 2 surfaces
 	def parallaxtrace2(self, sphericalrefraction1, sphericalrefraction2, outputplane, ray):
+		"""estimates the parallaxial focus through two sphericalrefraction lenses 
+		using the trace() method for two rays"""
 		objectchecker([sphericalrefraction1, sphericalrefraction2], outputplane, ray)
 		sphericalrefraction1.propagate_ray(self)
 		sphericalrefraction1.propagate_ray(ray)
@@ -85,8 +93,9 @@ class Ray:
 		self.plotparallax(ray)
 
 	def plot(self):
+		"""plot the ray's path (x vs. z)"""
 		z, y = [], []
-		for i in self.___points:
+		for i in self._points:
 			z.append(i[2]), y.append(i[1])
 		print z, y
 		plt.plot(z, y, color = "Blue")
@@ -96,11 +105,12 @@ class Ray:
 		plt.show()
 
 	def plotparallax(self, ray):
+		"""plot two rays' paths (x vs. z)"""
 		objectchecker(None, None, ray)
 		z, y, p, q = [], [], [], []
-		for i in self.__points:
+		for i in self._points:
 			z.append(i[2]), y.append(i[1])
-		for i in ray.__points:
+		for i in ray._points:
 			p.append(i[2]), q.append(i[1])
 		plt.plot(z, y, color = "Red")
 		plt.plot(p, q, color = "Blue")
@@ -110,6 +120,7 @@ class Ray:
 		plt.show()
 
 class OpticalElement:
+"""Any type of lens, outputplane or reflecting surface that propagates a ray or beam"""
 
 	def intercept(self, ray):
   		"""calculate the ray's intersection with the optical element"""
@@ -120,14 +131,15 @@ class OpticalElement:
   		raise NotImplementedError()
 
 class SphericalRefraction(OpticalElement):
+"""A spherical or plane shaped lens that refracts the ray or beam"""
 
 	def __init__(self, z0 = 0.0, c = 0.0, n1 = 1.0, n2 = 1.0, ar = 0.0):
-		self.z0 = z0
-		self.c = c
-		self.n1 = n1
-		self.n2 = n2
-		self.ar = ar
-		self.R = self.radius()
+		self._z0 = z0
+		self._c = c
+		self._n1 = n1
+		self._n2 = n2
+		self._ar = ar
+		self._r = self._radius()
 		self.s = self.surface()
 		self.centre = self.centre()
 		
@@ -135,74 +147,84 @@ class SphericalRefraction(OpticalElement):
 		# c = curvature
 
 	def __repr__(self):
-		return "Spherical refracting lens at z=%s of c=%s, n1/n2=%s, ar =%s" %(self.z0, self.c, self.n1/self.n2, self.ar)
+		return "Spherical refracting lens at z=%s of c=%s, n1/n2=%s, ar =%s" %(self._z0, self._c, self._n1/self._n2, self._ar)
 
 	def radius(self):
-		if self.c == 0:
+		if self._c == 0:
 			return float("inf")
 		else:
-			return abs(1/self.c)
+			return abs(1/self._c)
 
 	def surface(self):
-		if self.c < 0:
+		"""Defines the surface type as a string as an attribute accessible from the interface"""
+		if self._c < 0:
 			return "concave"
-		elif self.c > 0:
+		elif self._c > 0:
 			return "convex"
-		elif self.c == 0:
+		elif self._c == 0:
 			return "plane"
 
 	def centre(self):
 		if self.s == "convex":
-			return np.array([0, 0, self.z0 + self.R])
+			return np.array([0, 0, self._z0 + self._r])
 		elif self.s == "concave":
-			return np.array([0, 0, self.z0 - self.R])
+			return np.array([0, 0, self._z0 - self._r])
 		elif self.s == "plane":
 			return 0
 
 	def intercept(self, ray):
 		objectchecker(None, None, ray)
-		ar_z = np.sqrt(self.R**2 - self.ar**2) 
+		ar_z = np.sqrt(self._r**2 - self._ar**2) 
 		#ar_z = distance from aperture radius z intercept to centre of sphere
 		r = ray.p() - self.centre
 		r_mag = np.sqrt(sum(n**2 for n in r))
 		rdotk = np.dot(r, ray.k())
+		discriminant = rdotk**2 - r_mag**2 + self._r**2
 
-		if (rdotk**2 - r_mag**2 + self.R**2) < 0:
+		if discriminant < 0:
 			return None
 		else:
-			l1 = -rdotk + np.sqrt(rdotk**2 - r_mag**2 + self.R**2)
-			l2 = -rdotk - np.sqrt(rdotk**2 - r_mag**2 + self.R**2)
-			lplane = (self.z0 - ray.p()[2]) / ray.k()[2]
+			l1 = -rdotk + np.sqrt(rdotk**2 - r_mag**2 + self._r**2)
+			l2 = -rdotk - np.sqrt(rdotk**2 - r_mag**2 + self._r**2)
+			lplane = (self._z0 - ray.p()[2]) / ray.k()[2]
 
 		if self.s == "convex":
-			if (rdotk**2 - r_mag**2 + self.R**2) == 0:
+			if discriminant == 0:
+				"""if ray passes through aperture"""
 				if self.centre[2] - ar_z >= (ray.p() + -rdotk*ray.k())[2]:
 					return ray.p() + -rdotk*ray.k()
 				else:
 					return None
 			else:
+			"""Since convex, return the shortst length from ray point
+			if ray passes through the aperture"""
 				if self.centre[2] - ar_z >= (ray.p() + min(l1, l2)*ray.k())[2]:
 					return ray.p() + min(l1, l2)*ray.k()
 				else:
 					return None
 		elif self.s == "concave":
-			if (rdotk**2 - r_mag**2 + self.R**2) == 0:
+			if discriminant == 0:
+				"""if ray passes through aperture"""
 				if self.centre[2] + ar_z <= (ray.p() + -rdotk*ray.k())[2]:
 					return ray.p() + -rdotk*ray.k()
 				else:
 					return None
 			else:
+			"""Since concave, return the longest length from ray point if ray passes
+			through the aperture"""
 				if self.centre[2] + ar_z <= (ray.p() + max(l1, l2)*ray.k())[2]:
 					return ray.p() + max(l1, l2)*ray.k()
 				else:
 					return None
 		elif self.s == "plane":
-			if np.sqrt((ray.p() + lplane*ray.k())[0]**2 + (ray.p() + lplane*ray.k())[1]**2) <= self.ar:
+			"""if it passes through the aperture"""
+			if np.sqrt((ray.p() + lplane*ray.k())[0]**2 + (ray.p() + lplane*ray.k())[1]**2) <= self._ar:
 				return ray.p() + lplane*ray.k()
 			else:
 				return None
 
 	def unitsurfacenormal(self, ray):
+		"""Find the surface normal based on ray's intersection with a non-plane surface"""
 		objectchecker(None, None, ray)		
 		if self.s == "plane":
 			return np.array([0,0,-1])
@@ -215,7 +237,7 @@ class SphericalRefraction(OpticalElement):
 		objectchecker(None, None, ray)
 		n_unit = -self.unitsurfacenormal(ray)
 		k1 = ray.k() 
-		ref = self.n1/self.n2
+		ref = self._n1/self._n2
 		ndotk1 = np.dot(n_unit, k1)
 		if np.sin(np.arccos(ndotk1)) > (1/ref):
 			return None
@@ -233,16 +255,18 @@ class SphericalRefraction(OpticalElement):
 			print "Final Point: %s" %(ray.p()) + " and Final Direction: %s" %(ray.k())
 
 class OutputPlane(OpticalElement):
+"""A plane at the end of the ray or beam's path to record its final point after propagation
+through the other optical elements"""
 
 	def __init__(self, z = 0.0):
-		self.z_output = z
+		self._zoutput = z
 
 	def __repr__(self):
-		return "Output plane at z=%s" %(self.z_output)
+		return "Output plane at z=%s" %(self._zoutput)
 
 	def intercept(self, ray):
 		objectchecker(None, None, ray)
-		l = (self.z_output - ray.p()[2]) / ray.k()[2]
+		l = (self._zoutput - ray.p()[2]) / ray.k()[2]
 		return ray.p() + l*ray.k()
 
 	def propagate_ray(self, ray):
@@ -251,68 +275,81 @@ class OutputPlane(OpticalElement):
 		ray.append(p, None)
 		return "Final Point: %s" %(ray.p()) + "and Final Direction: %s" %(ray.k())
 
-class CollimatedBeam:
+#class SphericalReflection(OpticalElement):
 
-	def __init__(self, Ray, r = 10, n = 6):
-		self.n = n
-		self.r = r
-		self.Ray = Ray
-		self.x, self.y = self.Ray.p()[0], self.Ray.p()[1]
-		self.Beam, self.Beam__points = [self.Ray], [self.Ray.__points]
+
+class CollimatedBeam:
+"""Create a collimated beam from an input ray, a beam radius and the 
+number of radii within that radius"""
+
+	def __init__(self, ray, r = 10, n = 6):
+		self._n = n
+		self._r = r
+		self._ray = ray
+		self._x, self._y = self._ray.p()[0], self._ray.p()[1]
+		self._Beam, self._Beampoints = [self._ray], [self._ray._points]
 		self.raynumber = self.raynumber()
 		objectchecker(None, None, ray)
 
 	def __repr__(self):
-		return "Collimated Beam of radius = %s, density = %s, centred at (%s,%s)" % (self.r,self.d,self.x,self.y)
+		return "Collimated Beam of radius = %s, density = %s, centred at (%s,%s)" % (self._r,self.d,self._x,self._y)
 
 	def create(self):
-		for r in np.linspace(0, self.r, self.n, endpoint = True):
-			for i in np.linspace(0, 2*np.pi, (2*np.pi*r*(self.n-1))/self.r, endpoint = True):
-				self.Beam.append(Ray([self.x+r*np.cos(i), self.y + r*np.sin(i), 0], self.Ray.k()))
-		return self.Beam
+		for r in np.linspace(0, self._r, self._n, endpoint = True):
+			for i in np.linspace(0, 2*np.pi, (2*np.pi*r*(self._n-1))/self._r, endpoint = True):
+				self._Beam.append(Ray([self._x+r*np.cos(i), self._y + r*np.sin(i), 0], self._ray.k()))
+		return self._Beam
 
 	def raynumber(self):
-		return len(self.Beam)
+		"""the number of rays in the beam"""
+		return len(self._Beam)
 
 	def trace(self, sphericalrefraction, outputplane):
+		"""propagates and plots the beam through a sphericalrefracting 
+		surface and output plane"""
 		objectchecker([sphericalrefraction], outputplane)
-		self.create()
-		for i in self.Beam:
+		self._create()
+		for i in self._Beam:
 			sphericalrefraction.propagate_ray(i)
 			outputplane.propagate_ray(i)
-			self.Beam__points.append(i.__points)
+			self._Beampoints.append(i.vertices())
 		self.plot()
-		self.spotplot(outputplane.z_output)
-		print 'Diffraction Limit = %s' %(((outputplane.z_output - sphericalrefraction.z0)*680*10**(-9))/(2*self.r))
+		self.spotplot(outputplane.zoutput)
+		print 'Diffraction Limit = %s' %(((outputplane.zoutput - sphericalrefraction.z0)*680*10**(-9))/(2*self._r))
 
 	def trace2(self, sphericalrefraction1, sphericalrefraction2, outputplane):
-		self.create()
-		for i in self.Beam:
+		"""propagates and plots the beam through two sphericalrefraction lenses 
+		using the trace() method for two rays"""
+		self._create()
+		for i in self._Beam:
 			sphericalrefraction1.propagate_ray(i)
 			sphericalrefraction2.propagate_ray(i)
 			outputplane.propagate_ray(i)
-			self.Beam__points.append(i.__points)
+			self._Beampoints.append(i.vertices())
 		self.plot()
-		self.spotplot(outputplane.z_output)
-		print 'Diffraction Limit = %s' %(((outputplane.z_output - sphericalrefraction1.z0)*680*10**(-9))/(2*self.r))
+		self.spotplot(outputplane.zoutput)
+		print 'Diffraction Limit = %s' %(((outputplane.zoutput - sphericalrefraction1.z0)*680*10**(-9))/(2*self._r))
 
 	def plot(self):
+		"""plot the beam's path (y vs. z)"""
 		z_coords, y_coords = [], []
-		for list in self.Beam__points:
+		for list in self._Beampoints:
 			z, y = [], []
 			for i in list:
 				z.append(i[2]), y.append(i[1])
 			z_coords.append(z), y_coords.append(y)		
 		for z,y in zip(z_coords, y_coords):
 				plt.plot(z, y, color = "Blue")
-		plt.title('Beam Path for beam of %s rays, beam radius: %s' %(self.raynumber, self.r))
+		plt.title('Beam Path for beam of %s rays, beam radius: %s' %(self.raynumber, self._r))
 		plt.xlabel('z')
 		plt.ylabel('y')
 		plt.show()
 
 	def spotplot(self, z = 0):
+		"""plot the x-y distribution of rays in the beam at various z coordinates 
+		along the beam's path"""
 		coords, distances = [], []
-		for list in self.Beam__points:
+		for list in self._Beampoints:
 			for i in list:
 				if i[2] == z:
 					coords.append((i[0],i[1]))
@@ -320,7 +357,7 @@ class CollimatedBeam:
 			distances.append(np.sqrt(sum(n**2 for n in i)))
 		print 'Rms deviation = %s' %(np.sqrt(sum((n**2 for n in distances))/len(coords)))
 		plt.scatter(*zip(*coords), color = "Red")
-		plt.title('Spot Diagram at z = %s for beam of %s rays, beam radius: %s' %(z, self.raynumber, self.r))
+		plt.title('Spot Diagram at z = %s for beam of %s rays, beam radius: %s' %(z, self.raynumber, self._r))
 		plt.xlabel('x')
 		plt.ylabel('y')
 		plt.show()
